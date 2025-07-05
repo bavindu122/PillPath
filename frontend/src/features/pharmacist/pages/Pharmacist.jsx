c96c6bbc914b9f80b940a1a5c4c1592b92a731cb
SenujaUdugampola <2022cs204@stu.ucsc.cmb.ac.lk>
2025-07-05 17:26:11 +0530

    feat(pharmacist): implement OrderHistoryList page with order filtering, statistics, and related components

--- frontend/src/features/pharmacist/pages/Pharmacist.jsx
+++ frontend/src/features/pharmacist/pages/Pharmacist.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PharmacistDashboard from "./PharmacistDashboard"
 import ReviewPrescriptions from "./ReviewPrescriptions"
 import PrescriptionQueueList from "./PrescriptionQueueList"
+import OrderHistoryList from "./OrderHistoryList"
 import { Route, Routes } from 'react-router-dom'
 
 const Pharmacist = () => {
@@ -11,6 +12,7 @@ const Pharmacist = () => {
             <Route path="/" element={<PharmacistDashboard />} />
             <Route path="/dashboard" element={<PharmacistDashboard />} />
             <Route path="/queue" element={<PrescriptionQueueList />} />
+            <Route path="/orders" element={<OrderHistoryList />} />
             <Route path="/review/:prescriptionId" element={<ReviewPrescriptions />} />
         </Routes>
     </div>
