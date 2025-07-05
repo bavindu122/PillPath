5cbc7236d25e3c227e5c72a771a5125b7a9616d3
Sanuthma Sandanaki Munasinghe <sanumunasinghe02@gmail.com>
2025-07-05 11:38:05 +0530

    fix(sidebar):fix navigation

--- frontend/src/features/customer/components/CustomerSidebar.jsx
+++ frontend/src/features/customer/components/CustomerSidebar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { assets } from "../../../assets/assets";
 import ProfileModal from "../components/ProfileModal";
+import { NavLink } from "react-router-dom";
 
 const CustomerSidebar = () => {
           const [showProfileModal, setShowProfileModal] = useState(false);
@@ -9,8 +10,13 @@ const CustomerSidebar = () => {
     <>
       <aside className="w-64 min-h-screen bg-white/80 shadow-xl border-r border-white/30 flex flex-col py-8 px-4 z-20">
         <nav className="flex flex-col gap-2">
+          
           <button className="flex items-center gap-3 px-4 py-3 rounded-lg bg-primary/10 text-primary font-semibold focus:outline-none">
-            Dashboard
+            <NavLink
+                            to="/customer-profile"
+                            className="cursor-pointer hover:text-pButton">
+                        Dashboard
+                      </NavLink>
           </button>
           <button className="flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-primary/10 text-gray-700">
             Past orders
