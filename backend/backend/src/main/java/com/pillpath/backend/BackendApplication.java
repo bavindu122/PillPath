f811c7015ae304eb9528e270d2f2160ddd0c9ac9
tanurims <2022cs194@stu.ucsc.cmb.ac.lk>
2025-07-13 21:05:58 +0530

    connect with database

---
+++ backend/backend/src/main/java/com/pillpath/backend/BackendApplication.java
@@ -0,0 +1,26 @@
+package com.pillpath.backend;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure()
+        .directory("backend/backend")  // <-- Correct path now!
+        .filename(".env")
+        .load();
+
+        System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+        System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+        System.setProperty("jwt.secret", dotenv.get("JWT_SECRET"));
+
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
