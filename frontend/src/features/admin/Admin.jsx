7f6128d236a03913520d23d78e321f62ef668368
tanurims <2022cs194@stu.ucsc.cmb.ac.lk>
2025-07-17 15:15:24 +0530

    feat:pharmacy

--- frontend/src/features/admin/Admin.jsx
+++ frontend/src/features/admin/Admin.jsx
@@ -4,6 +4,7 @@ import AdminLayout from './components/AdminLayout';
 import { Navigate } from 'react-router-dom';
 import Overview from './pages/Overview';
 import Customers from './pages/Customers';
+import Pharmacies from './pages/Pharmacies';
 
 const Admin = () => {
   return (
@@ -12,6 +13,7 @@ const Admin = () => {
         <Route path="/" element={<Navigate to="overview" replace />} /> 
         <Route path="overview" element={<Overview />} />
         <Route path="customers" element={<Customers />} />
+        <Route path="pharmacies" element={<Pharmacies />} />
 
       </Routes>
       
